68370efbd3fedc3622c9d9af6ea2e8ca1329ff51
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-18 11:43:54 -0700

    Script for examining command invocations.

--- scripts/print_app_query_evals.py
+++ scripts/print_app_query_evals.py
@@ -1,11 +1,26 @@
 
+from collections import defaultdict
 from queryanalysis.db import connect_db
-from queryutils.parse import extract_stages_with_cmd
+from queryutils.parse import extract_stages_with_cmd, parse_query
 
 db = connect_db()
 source = "storm"
 cursor = db.execute("SELECT text, source FROM queries WHERE source!=?", [source])
+templates = defaultdict(int)
+failed = 0
 for (text, source) in cursor.fetchall():
-    print extract_stages_with_cmd("eval", str(text))
-
+    for s in extract_stages_with_cmd("eval", str(text)):
+        try:
+            p = parse_query(s)
+        except:
+            failed += 1
+            print "Failed: ", s
+            pass
+        if p:
+            templates[p.template().flatten()] += 1
 db.close()
+
+print "Total failed:", failed
+for (template, count) in templates.iteritems():
+    print count, template
+
