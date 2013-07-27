988d304c3ce2147455e5630e3351256745256135
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-27 14:31:42 -0700

    Almost done with code that generates LSI space.

--- queryanalysis/lsi/lsi.py
+++ queryanalysis/lsi/lsi.py
@@ -1,24 +1,50 @@
 
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 from queryanalysis.db import connect_db
-from queryanalysis.lsi.contexts import *
+
+from scipy.sparse import coo_matrix
 
 import sys
 
 def main():
-    # parse options
+    parser = ArgumentParser(description="")
+    parser.add_argument("tablename", metavar="TABLE",
+                        type=str, nargs=1,
+                        help="the name of the table where the \
+                               fingerprings and functions are stored")
+    parser.add_arguments("package", metavar="PACKAGE",
+                        type=str, nargs=1,
+                        help="the name of the package where the \
+                                module containing the Fingerprint \
+                                and Function class definitions"
+    parser.add_argument("--output", "-o", dest="output")
+    
+    args = parser.parse_args()
 
-def tally_frequencies():
-    db = connect_db()
-    tally = {}
-    cursor = db.execute("SELECT fingerprint, function FROM lsi")
+    import_module('contexts', package=args.package)
+
+    output = args.output
+    if not output:
+        output = args.tablename
+    output = ''.join([output, '.lsi'])    
+    run(args.tablename, output)
+
+def run(tablename, output):
     column_indices = defaultdict(counter().next)
+    frequencies = tally_frequencies(tablename)
+    sparse_frequencies = get_sparse_matrix(frequencies, column_indices)
+
+def tally_frequencies(tablename):
+    db = connect_db()
+    tally = OrderedDict()
+    cursor = db.execute("SELECT fingerprint, function FROM ?", [tablename])
     for (fingerprint, function) in cursor.fetchall():
         fingerprint = Fingerprint.deserialize(fingerprint)
         function = Function.deserialize(function)
         increment(fingerprint, function, tally)
     db.close()
+    row_indices = zip(tally.keys(), range(len(tally.keys()))
     return tally
 
 def counter():
@@ -33,14 +59,21 @@ def increment(fingerprint, function, tally):
     tally[fingerprint][function] += 1.
 
 def get_sparse_matrix(frequencies, column_indices):
-    s = "row,col,cnt"
+    rows = []
+    cols = []
+    cnts = []
     row_idx = 0
     for (row_key, column_entries) in frequencies.iteritems():
         for (column_key, frequency) in column_entries.iteritems():
             col_idx = column_indices[column_key]
-            tuple = ",".join([str(row_idx), str(col_idx), str(frequency)])
-            s = '\n'.join([s, tuple])
+            rows.append(row_idx)
+            cols.append(col_idx)
+            cnts.append(frequency)
         row_idx += 1
-    return s
+    return coo_matrix(cnts, rows, cols)
+    
+def svd(matrix, desired_singular_values):
+    (left, S, right, iters, prods) = irlb(matrix, desired_singular_values)
+    return (left, right)
 
 main()
