ffa5064b2199a830f2f6ac6190e6b7d7b94f02ce
jaytlin <jessica.lin@berkeley.edu>
2013-07-31 16:56:50 -0700

    Added 'onefieldfun' experiment as well as 'print_queries_by_command' script which was used to generate data to query the lsi model.

---
+++ scripts/print_queries_by_command.py
@@ -0,0 +1,41 @@
+from queryanalysis.db import connect_db
+from queryutils.parse import extract_stages_with_cmd
+from queryanalysis.lsi.experiments.onefieldfun.contexts import *
+
+import string
+import json
+import splparser
+import queryutils.parse as parser
+
+db = connect_db()
+cur = db.cursor()
+
+cur.execute("select text from queries where source='unix'")
+rows = cur.fetchall()
+to_query = []
+for row in rows:
+    for col in row:
+#         # PRINT SCHEMA
+#         try:
+#             p = splparser.parse(str(col))
+#             print p.schema()
+#         except:
+#             pass
+        s = str(col)      
+        sort_cmd = extract_stages_with_cmd('sort',s)
+        if sort_cmd:
+            s = sort_cmd[0]
+            try:
+                p = splparser.parse(s)
+                tuples = lsi_tuples_from_parsetree(p)
+                to_query.append(tuples[0][0])
+                print to_query
+                
+            except Exception, e:
+                print "The following error for command: %s" % s
+                print str(e)
+                print "\n"
+                
+print json.dumps([f.jsonify() for f in to_query])
+        
+        
