f6fef87fbd3e90cff87e863bc78603c5e93ec979
salspaugh <saraalspaugh@gmail.com>
2013-08-03 23:23:06 -0700

    Changed tabs to spaces in contexts.py.

--- queryanalysis/lsi/experiments/onefieldfun/contexts.py
+++ queryanalysis/lsi/experiments/onefieldfun/contexts.py
@@ -40,70 +40,70 @@ class Fingerprint(object):
         
     def distance(self,other):
         if self == other:
-        	return 0
+            return 0
         else:
-        	# raw_dist = get_raw_dist(self.raw_argument, other.raw_argument)
-        	# canon_dist = get_canon_dist(self.canonicalized_argument, other.canonicalized_argument)
-        	# role_dist = get_role_dist(self.role, other.role)
-        	# type_dist = get_type_dist(self.type, other.type)
-        	# dtype_dist = get_dtype_dist(self.datatype, other.datatype)
-        	# if self.previousfn and other.previousfn:
-        	#         		prevfn_dist = get_prevfn_dist(self.previousfn, other.previousfn)
-        	# 			else:
-			prevfn_dist = 0
-			return average_dists(raw_dist, canon_dist, role_dist, type_dist, dtype_dist, prevfn_dist)
-			
-	def average_dists(self,raw_dist, canon_dist, role_dist, type_dist, dtype_dist, prevfn_dist):
-		sum_dists = W_RAW*raw_dist+W_CANON*canon_dist+W_ROLE*role_dist+W_TYPE*type_dist+W_DTYPE*dtype_dist+W_PREVFN*prevfn_dist
-		return sum_dists/6.0
-		
+            # raw_dist = get_raw_dist(self.raw_argument, other.raw_argument)
+            # canon_dist = get_canon_dist(self.canonicalized_argument, other.canonicalized_argument)
+            # role_dist = get_role_dist(self.role, other.role)
+            # type_dist = get_type_dist(self.type, other.type)
+            # dtype_dist = get_dtype_dist(self.datatype, other.datatype)
+            # if self.previousfn and other.previousfn:
+            #                 prevfn_dist = get_prevfn_dist(self.previousfn, other.previousfn)
+            #             else:
+            prevfn_dist = 0
+            return average_dists(raw_dist, canon_dist, role_dist, type_dist, dtype_dist, prevfn_dist)
+            
+    def average_dists(self,raw_dist, canon_dist, role_dist, type_dist, dtype_dist, prevfn_dist):
+        sum_dists = W_RAW*raw_dist+W_CANON*canon_dist+W_ROLE*role_dist+W_TYPE*type_dist+W_DTYPE*dtype_dist+W_PREVFN*prevfn_dist
+        return sum_dists/6.0
+        
     def get_raw_dist(self,raw1,raw2):
-    	return (0 if raw1 == raw2 else 1)
-		
+        return (0 if raw1 == raw2 else 1)
+        
     def get_canon_dist(self,canon1,canon2):
-    	return editdist.distance(canon1, canon2)
-		
+        return editdist.distance(canon1, canon2)
+        
     def get_role_dist(self,role1,role2):
-    	config = configparser.ConfigParser()
-		config.read(ROLES_FILE)
-		if role1 in config and role2 in config:
-			return config[role1][role2]
-		else:
-			print "role1 in config: %s" % (role1 in config)
-			print "role2 in config: %s" % (role2 in config)
-			return 0
-		
+        config = configparser.ConfigParser()
+        config.read(ROLES_FILE)
+        if role1 in config and role2 in config:
+            return config[role1][role2]
+        else:
+            print "role1 in config: %s" % (role1 in config)
+            print "role2 in config: %s" % (role2 in config)
+            return 0
+        
     def get_type_dist(self,type1,type2):
-    	config = configparser.ConfigParser()
-		config.read(TYPES_FILE)
-		if type1 in config and type2 in config:
-			return config[type1][type2]
-		else:
-			print "Type1 in config: %s" % (type1 in config)
-			print "Type2 in config: %s" % (type2 in config)
-			return 0
-						
+        config = configparser.ConfigParser()
+        config.read(TYPES_FILE)
+        if type1 in config and type2 in config:
+            return config[type1][type2]
+        else:
+            print "Type1 in config: %s" % (type1 in config)
+            print "Type2 in config: %s" % (type2 in config)
+            return 0
+                        
     def get_dtype_dist(self,dtype1, dtype2):
-    	return 0 # ALL UNKNOWN FOR NOW	
-		
+        return 0 # ALL UNKNOWN FOR NOW    
+        
     def get_prevfn_dist(self,prevfn1, prevfn2):
-    	return 0 # ALL UNKNOWN FOR NOW	
-			
+        return 0 # ALL UNKNOWN FOR NOW    
+            
     def jsonify(self):
         d = self.__dict__
         if self.previousfn:
             d['previousfn'] = self.previousfn.jsonify()
         return d
-		
+        
     class FingerprintEncoder(json.JSONEncoder):
         def default(self, obj):
             if isinstance(obj, Fingerprint):
                 return obj.jsonify()
             return json.JSONEncoder.default(self, obj)
-        	
+            
     def serialize(self, **kwargs):
         return json.dumps(self, cls=self.FingerprintEncoder, **kwargs)
-			
+            
     @staticmethod
     def deserialize(d):
         if not type(d) == type({}): 
@@ -119,7 +119,7 @@ class Fingerprint(object):
         return f
 
 class Function(object):
-			
+            
     def __init__(self):
         self.parsetreenode = None
         self.signature = None
